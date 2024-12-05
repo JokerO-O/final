09806ffe393350ae393c47b5a6044d4a11dc72b4
JokerO-O <universidaditla4@gmail.com>
2024-12-05 14:08:08 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-SECRET_KEY = 'clave_secreta_segura'
+SECRET_KEY = '2511040506'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Configuración de Flask-Session
-SESSION_TYPE = 'filesystem'  # Otras opciones: 'redis', 'memcached', etc.
-SESSION_PERMANENT = False  # Para sesiones no permanentes
-SESSION_USE_SIGNER = True  # Opcional, para firmar las sesiones
-SESSION_COOKIE_NAME = 'session'
-
