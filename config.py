923b45ec2c4d6fab15adcb0757ffcb4652fc8e6f
JokerO-O <universidaditla4@gmail.com>
2024-12-05 04:54:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ SECRET_KEY = 'clave_secreta_segura'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SESSION_TYPE = 'filesystem'
+SESSION_PERMANENT = False  # Dependiendo de si quieres que la sesión sea permanente
+SESSION_USE_SIGNER = True  # Opcional, para firmar las sesiones
+SESSION_COOKIE_NAME = 'your_session_cookie_name'  # Define un nombre para la cookie de sesión
