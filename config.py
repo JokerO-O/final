fd09edc369d3963e4171293da79efec8a318316f
JokerO-O <universidaditla4@gmail.com>
2024-12-05 05:07:50 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 SECRET_KEY = 'clave_secreta_segura'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SESSION_TYPE = 'filesystem'
-SESSION_PERMANENT = False  # Dependiendo de si quieres que la sesión sea permanente
+
+# Configuración de Flask-Session
+SESSION_TYPE = 'filesystem'  # Otras opciones: 'redis', 'memcached', etc.
+SESSION_PERMANENT = False  # Para sesiones no permanentes
 SESSION_USE_SIGNER = True  # Opcional, para firmar las sesiones
-SESSION_COOKIE_NAME = 'your_session_cookie_name'  # Define un nombre para la cookie de sesión
+SESSION_COOKIE_NAME = 'your_session_cookie_name'  # Nombre de la cookie
